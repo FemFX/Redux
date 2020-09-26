b9c90c6af1ab1705214123f856c081b31635b263
FemFX <ignedko34@gmail.com>
2020-09-26 18:24:31 +0300

    Create actions.js

---
+++ src/redux/actions.js
@@ -0,0 +1,25 @@
+import { DECREMENT, INCREMENT, ASYNC_INCREMENT,CHANGE_THEME } from './types';
+
+export const increment = () => {
+  return {
+    type: INCREMENT,
+  };
+};
+export const decrement = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+export const asyncIncrement = () => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch({ type: ASYNC_INCREMENT });
+    }, 2000);
+  };
+};
+export const changeTheme = (newTheme) => {
+  return {
+    type: CHANGE_THEME,
+    payload : newTheme
+  };
+};
